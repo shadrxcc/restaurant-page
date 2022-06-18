9d621de72599c77c1ffe32f494a1bef4875c8cb4
shadrach akaade <akaadesm@gmail.com>
2022-06-18 22:27:15 +0100

    backdate contacttab

--- src/contactTab.js
+++ src/contactTab.js
@@ -6,31 +6,7 @@ function contactTab() {
   const section = document.createElement('div')
   
 
-  const h1 = document.createElement('h1')
-  h1.textContent = "Contact Us"
-  h1.classList.add('contact')
-  section.appendChild(h1)
-
-  const av = document.createElement('img')
-  av.src = avatar
-  av.classList.add('profile')
-  section.appendChild(av)
-
-
-  const contactDetails = (tag, link, anchorText) => {
-    const div = document.createElement('div')
-    div.classList.add('contact-box')
-    div.textContent = `${tag}:`
-
-    const a = document.createElement('a')
-    a.classList.add('contact-link')
-    a.href = link
-    a.textContent = anchorText
-    div.appendChild(a)
-
-    section.appendChild(div)
-    
-  }
+  
 
   contactDetails ('Email', 'mailto:akaadesm@gmail.com', 'akaadesm@gmail.com')
   contactDetails ('Github', 'connectto:github.com/shadrxcc', 'github.com/shadrxcc')
